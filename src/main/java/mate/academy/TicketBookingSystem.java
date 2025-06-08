2bbfaf4a15aa2092b5d2e1be721e27df5a8d0281
Lentezz <yaroslav.golovan@gmail.com>
2025-06-09 00:42:30 +0300

    Implement semaphore

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,28 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private int seatsAvailable;
+    private final Semaphore seatsAvailableSemaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        seatsAvailable = totalSeats;
+        seatsAvailableSemaphore = new Semaphore(1);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            seatsAvailableSemaphore.acquire();
+            if (seatsAvailable != 0) {
+                seatsAvailable--;
+                return new BookingResult(user, true, "Booking successful.");
+            }
+            return new BookingResult(user, false, "No seats available.");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            seatsAvailableSemaphore.release();
+        }
     }
 }
